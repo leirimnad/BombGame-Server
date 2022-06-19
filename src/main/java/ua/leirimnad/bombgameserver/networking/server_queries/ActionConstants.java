dd92974c65a80a7071f3b64f475d680012b30fb4
AlwaysSayNo <allbitxxx@gmail.com>
2022-06-19 11:19:15 +0300

    Added ActionConstants for action names.

---
+++ src/main/java/ua/leirimnad/bombgameserver/networking/server_queries/ActionConstants.java
@@ -0,0 +1,14 @@
+package ua.leirimnad.bombgameserver.networking.server_queries;
+
+public interface ActionConstants {
+
+    String GET_TABLE_LIST = "GET_TABLE_LIST";
+    String CREATE_TABLE = "CREATE_TABLE";
+    String JOIN_TABLE = "JOIN_TABLE";
+    String LEAVE_TABLE = "LEAVE_TABLE";
+    String START_GAME = "START_GAME";
+    String UPDATE_WORD = "UPDATE_WORD";
+    String CONFIRM_WORD = "CONFIRM_WORD";
+    String PING = "PING";
+
+}
