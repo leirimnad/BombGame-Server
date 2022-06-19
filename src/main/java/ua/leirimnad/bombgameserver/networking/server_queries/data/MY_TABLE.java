0d55937070e28aa8555f1ca9ca7b60a8b1c2f891
AlwaysSayNo <allbitxxx@gmail.com>
2022-06-19 22:11:11 +0300

    Added ability to return table.

--- src/main/java/ua/leirimnad/bombgameserver/networking/server_queries/data/MY_TABLE.java
+++ src/main/java/ua/leirimnad/bombgameserver/networking/server_queries/data/MY_TABLE.java
@@ -3,10 +3,10 @@ package ua.leirimnad.bombgameserver.networking.server_queries.data;
 import ua.leirimnad.bombgameserver.networking.server_queries.ServerQueryData;
 import ua.leirimnad.bombgameserver.tables.Table;
 
-public class GET_MY_TABLE implements ServerQueryData {
+public class MY_TABLE implements ServerQueryData {
     public final Table table;
 
-    public GET_MY_TABLE(Table table){
+    public MY_TABLE(Table table){
         this.table = table;
     }
 }
